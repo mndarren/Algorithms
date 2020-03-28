cc52dafc8d80831b65540d20bb894fdb386dd54c
mndarren <mndarren@gmail.com>
2020-03-27 22:07:47 -0500

    updated TwoSum

--- src/Algorithms/Main.java
+++ src/Algorithms/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		int[] numbers = new int[] {2, 3, 5, 7, 9, 11, 23, 15};
 		//find the target 18
 		TwoSum twoSum = new TwoSum();
-		int[] positions = twoSum.twoSumHashTable(numbers, 18);
+		int[] positions = twoSum.twoSumPointers(numbers, 18);
 		
 		System.out.println("Found two sum: " + positions[0] + ", " + positions[1]);
 	}
--- src/Algorithms/TwoSum.java
+++ src/Algorithms/TwoSum.java
@@ -1,6 +1,10 @@
 package Algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @implSpec Given an array of integers, 
@@ -27,5 +31,55 @@ public class TwoSum {
 			h.put(key1, i);
 		}
 		throw new IllegalArgumentException("No two sum solution");
+	}
+	
+	/**
+	 * @apiNote Sort & Pointers: Time - O(N), Space - O(N)
+	 * @param numbers
+	 * @param target
+	 * @return the index positions
+	 */
+	public int[] twoSumPointers(int[] numbersIn, int target) {
+		int[] index = new int[2];
+		int[] numbers = Arrays.copyOf(numbersIn, numbersIn.length);
+		// Sorting O(NlogN)
+		Arrays.sort(numbers);
+		int lo = 0, hi = numbers.length - 1;
+	    while (lo < hi) {
+	        int sum = numbers[lo] + numbers[hi];
+	        if (sum > target)       hi--;
+	        else if (sum < target)  lo++;
+	        else                    break;
+	    }
+	    // search index
+	    for (int i = 0; i < numbers.length; i++) {
+	    	if (numbersIn[i] == numbers[lo]) index[0] = i;
+	    }
+	    for (int i = 0; i < numbers.length; i++) {
+	    	if (numbersIn[i] == numbers[hi] && i != index[0]) index[1] = i;
+	    }
+	    if (index.length == 2) return new int[] {index[0] + 1, index[1]+ 1};
+	    throw new IllegalArgumentException("No two sum solution");
+	}
+	
+	/**
+	 * @apiNote HashTable: Time - O(N), Space - O(N)
+	 * @param numbers
+	 * @param target
+	 * @return the index positions
+	 */
+	public List<List<Integer>> twoSumMutiplePairs(int[] numbers, int target) {
+		List<List<Integer>> listSet = new ArrayList<List<Integer>>();
+		int n = numbers.length;
+		if (n < 2) return listSet;
+		
+		Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+		for (int i = 0; i < n; i++) {
+			int key1 = numbers[i], key2 = target - numbers[i];
+			
+		}
+		
+	    if (listSet.size() > 0) return listSet;
+	    throw new IllegalArgumentException("No two sum solution");
 	}
 }
